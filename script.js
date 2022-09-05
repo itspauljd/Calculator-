ddd51de784b90432021f2f4ea68dc30473a7ccdf
Paul Park <paulparkjd@gmail.com>
2022-09-05 16:12:46 -0700

    Added styles to display div

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function addition(num1, num2) {
-    num1 + num2     
+    num1 + num2
 }
 function subtraction(num1, num2) {
     num1 - num2
@@ -8,6 +8,18 @@ function multiplication(num1, num2) {
     num1 * num2
 }
 function division(num1, num2) {
-    num1 / num2 
+    num1 / num2
+}
+
+function operate(operator, num1, num2) {
+    if (operator == 'addition') {
+        addition(num1, num2);
+    } else if (operator = 'subtraction') {
+        subtraction(num1, num2);
+    } else if (operator = 'multiplication') {
+        multiplication(num1, num2);
+    } else if (operator = 'division') {
+        division (num1, num2);
+    }
 }
 
