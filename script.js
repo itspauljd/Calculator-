152d1bc39371def3447a6838690d1889aea8e140
Paul Park <paulparkjd@gmail.com>
2022-09-05 16:54:09 -0700

    Added event listeners to display keys

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const numberKeys = document.getElementsByClassName('num');
+
+
+// operator functions 
 function addition(num1, num2) {
     num1 + num2
 }
@@ -11,6 +15,7 @@ function division(num1, num2) {
     num1 / num2
 }
 
+// operate function 
 function operate(operator, num1, num2) {
     if (operator == 'addition') {
         addition(num1, num2);
@@ -23,3 +28,11 @@ function operate(operator, num1, num2) {
     }
 }
 
+const numKeyArray = Array.from(numberKeys);
+numKeyArray.forEach(key => {
+    key.addEventListener('click', display)
+})
+
+function display() {
+    console.log('hi')
+}
